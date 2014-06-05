fa7c4a26aa0a40baffaeafc04e60e2bf7f87a462
Zhifeng Wang <wzfnju@gmail.com>
2014-06-05 12:19:30 +0800

    Update resizable.js

--- resizable.js
+++ resizable.js
@@ -1,143 +1,142 @@
-var wzf=angular.module("wzf");
-wzf.module('my-app',[])
-	.directive("resizable",function($document){
-		var startx=0,starty=0,x=0,y=0;
-		var link=function($scope,$element,$attr){
-			$element.css({
-				top:"100px",
-				overflow: "hidden",
-				display:"block",
-				position:"absolute",
-				border:'1ps solid red',
-				backgroundColor:'lightgrey'
-			});
-			
-			var top=angular.element("<div style='width:100%; border-top:2px solid black;  height:1px;'></div>");
+var wzf=angular.module("wzf",[]);
+wzf.directive("resizable",function($document){
+	var startx=0,starty=0,x=0,y=0;
+	var link=function($scope,$element,$attr){
+		$element.css({
+			top:"100px",
+			overflow: "hidden",
+			display:"block",
+			position:"absolute",
+			border:'1ps solid red',
+			backgroundColor:'lightgrey'
+		});
+		
+		var top=angular.element("<div style='width:100%; border-top:2px solid black;  height:1px;'></div>");
+		top.css({
+			position:"absolute",
+			top:"0px",
+			left:"0px"
+		});
+		top.bind("mouseover",function($event){
 			top.css({
-				position:"absolute",
-				top:"0px",
-				left:"0px"
+				cursor:"n-resize"
 			});
-			top.bind("mouseover",function($event){
-				top.css({
-					cursor:"n-resize"
-				});
-			}).bind("mousedown",function($event){
-				y=$($element).height();
-				starty=$event.clientY;
-				$document.bind('mousemove',mousemove1);
-				$document.bind('mouseup',mouseup1);
-				$event.stopPropagation();
-			});
-			$element.append(top);
+		}).bind("mousedown",function($event){
+			y=$($element).height();
+			starty=$event.clientY;
+			$document.bind('mousemove',mousemove1);
+			$document.bind('mouseup',mouseup1);
+			$event.stopPropagation();
+		});
+		$element.append(top);
+		
+		function mousemove1($event){
+			yy=y-($event.clientY-starty);
+			$element.css({
+				height:yy+'px',
+				top:$event.clientY+'px'
+			}); 
 			
-			function mousemove1($event){
-				yy=y-($event.clientY-starty);
-				$element.css({
-					height:yy+'px',
-					top:$event.clientY+'px'
-				}); 
-				
-			}
-			function mouseup1(){
-				$document.unbind('mousemove',mousemove1);
-				$document.unbind('mouseup',mouseup1);
-			}
-			var bottom=angular.element("<div style='width:100%; border-bottom:2px solid black; height:1px;'></div>");
+		}
+		function mouseup1(){
+			$document.unbind('mousemove',mousemove1);
+			$document.unbind('mouseup',mouseup1);
+		}
+		var bottom=angular.element("<div style='width:100%; border-bottom:2px solid black; height:1px;'></div>");
+		bottom.css({
+			position:"absolute",
+			bottom:"0px",
+			left:"0px"
+		});
+		bottom.bind("mouseover",function($event){
 			bottom.css({
-				position:"absolute",
-				bottom:"0px",
-				left:"0px"
+				cursor:"n-resize"
 			});
-			bottom.bind("mouseover",function($event){
-				bottom.css({
-					cursor:"n-resize"
-				});
-			}).bind("mousedown",function($event){
-				y=$($element).height();
-				starty=$event.clientY;
-				$document.bind('mousemove',mousemove2);
-				$document.bind('mouseup',mouseup2);
-				$event.stopPropagation();
-			});
-			$element.append(bottom);
-			function mousemove2($event){
-				yy=y+($event.clientY-starty);
-				$element.css({
-					height:yy+'px'
-				}); 
-			}
-			function mouseup2(){
-				$document.unbind('mousemove',mousemove2);
-				$document.unbind('mouseup',mouseup2);
-			}
-			
-			var left=angular.element("<div style='width:1px; border-left:2px solid black; height:100%;'></div>");
+		}).bind("mousedown",function($event){
+			y=$($element).height();
+			starty=$event.clientY;
+			$document.bind('mousemove',mousemove2);
+			$document.bind('mouseup',mouseup2);
+			$event.stopPropagation();
+		});
+		$element.append(bottom);
+		function mousemove2($event){
+			yy=y+($event.clientY-starty);
+			$element.css({
+				height:yy+'px'
+			}); 
+		}
+		function mouseup2(){
+			$document.unbind('mousemove',mousemove2);
+			$document.unbind('mouseup',mouseup2);
+		}
+		
+		var left=angular.element("<div style='width:1px; border-left:2px solid black; height:100%;'></div>");
+		left.css({
+			position:"absolute",
+			top:"0px",
+			left:"0px"
+		});
+		left.bind("mouseover",function($event){
 			left.css({
-				position:"absolute",
-				top:"0px",
-				left:"0px"
+				cursor:"e-resize"
 			});
-			left.bind("mouseover",function($event){
-				left.css({
-					cursor:"e-resize"
-				});
-			}).bind("mousedown",function($event){
-				x=$($element).width();
-				startx=$event.clientX;
-				$document.bind('mousemove',mousemove3);
-				$document.bind('mouseup',mouseup3);
-				$event.stopPropagation();
-			});
-			$element.append(left);
+		}).bind("mousedown",function($event){
+			x=$($element).width();
+			startx=$event.clientX;
+			$document.bind('mousemove',mousemove3);
+			$document.bind('mouseup',mouseup3);
+			$event.stopPropagation();
+		});
+		$element.append(left);
+		
+		function mousemove3($event){
+			xx=x-($event.clientX-startx);
+			$element.css({
+				width:xx+'px',
+				left:$event.clientX+'px'
+			}); 
 			
-			function mousemove3($event){
-				xx=x-($event.clientX-startx);
-				$element.css({
-					width:xx+'px',
-					left:$event.clientX+'px'
-				}); 
-				
-			}
-			function mouseup3(){
-				$document.unbind('mousemove',mousemove3);
-				$document.unbind('mouseup',mouseup3);
-			}
-			
-			
-			var right=angular.element("<div style='width:1px; border-left:2px solid black; height:100%;'></div>");
+		}
+		function mouseup3(){
+			$document.unbind('mousemove',mousemove3);
+			$document.unbind('mouseup',mouseup3);
+		}
+		
+		
+		var right=angular.element("<div style='width:1px; border-left:2px solid black; height:100%;'></div>");
+		right.css({
+			position:"absolute",
+			top:"0px",
+			right:"0px"
+		});
+		right.bind("mouseover",function($event){
 			right.css({
-				position:"absolute",
-				top:"0px",
-				right:"0px"
+				cursor:"e-resize"
 			});
-			right.bind("mouseover",function($event){
-				right.css({
-					cursor:"e-resize"
-				});
-			}).bind("mousedown",function($event){
-				x=$($element).width();
-				startx=$event.clientX;
-				$document.bind('mousemove',mousemove4);
-				$document.bind('mouseup',mouseup4);
-				$event.stopPropagation();
-			});
-			$element.append(right);
+		}).bind("mousedown",function($event){
+			x=$($element).width();
+			startx=$event.clientX;
+			$document.bind('mousemove',mousemove4);
+			$document.bind('mouseup',mouseup4);
+			$event.stopPropagation();
+		});
+		$element.append(right);
+		
+		function mousemove4($event){
+			xx=x+($event.clientX-startx);
+			$element.css({
+				width:xx+'px'
+			}); 
 			
-			function mousemove4($event){
-				xx=x+($event.clientX-startx);
-				$element.css({
-					width:xx+'px'
-				}); 
-				
-			}
-			function mouseup4(){
-				$document.unbind('mousemove',mousemove4);
-				$document.unbind('mouseup',mouseup4);
-			}
-			
-		};
-		return {
-			link:link
-		};
-	});
+		}
+		function mouseup4(){
+			$document.unbind('mousemove',mousemove4);
+			$document.unbind('mouseup',mouseup4);
+		}
+		
+	};
+	return {
+		link:link
+	};
+});
